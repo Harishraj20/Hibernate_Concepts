3ec46bfc253c43d145e4461154f5da03be7121f2
Harishraj20 <sharish29102002@gmail.com>
2024-10-16 22:39:17 +0530

    16-10-2024

--- criteria/src/main/java/com/example/executeCriteria.java
+++ criteria/src/main/java/com/example/executeCriteria.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public class executeCriteria {
     public static void main(String[] args) {
-        // for (int i = 1; i <= 20; i++) {
-        //     Employee emp = new Employee("Employee" + i, i, (i % 10) + 1, "Software Developer", 50000 + (i * 1000));
-        //                 methodImplementation.insertEmployee(emp);
-        // }
-        // System.out.println("Employee saved successfully");
+        for (int i = 1; i <= 20; i++) {
+            Employee emp = new Employee("Employee" + i, i, (i % 10) + 1, "Software Developer", 50000 + (i * 1000));
+                        methodImplementation.insertEmployee(emp);
+        }
+        System.out.println("Employee saved successfully");
 
         List <Employee> employees = methodImplementation.getEmployees(65000);
 
@@ -36,36 +36,33 @@ public class executeCriteria {
         }
 
 
+        List<Object[]> employeeData = methodImplementation.employeeRange();
+        System.out.println("List of Names of the employee role:");
+    
+        for (Object[] empData : employeeData) {
+            String name = (String) empData[0];
+            Integer employeeId = (Integer) empData[1];
+            String role = (String) empData[2];
+    
+            System.out.println("Name: " + name + ", ID: " + employeeId + ", Role: " + role);
+        }
+
+
+
+        List<Object[]> groupedInfo = methodImplementation.groupByExperience();
+
+        for (Object[] info : groupedInfo) {
+            int experience = (int) info[0];
+            long count = (long) info[1]; 
+
+            System.out.println("Experience: " + experience + ", Count: " + count);
+        }
+
+        List <Employee> employee_in = methodImplementation.betweenOperator();
+        System.out.println("Employee with Experience greater than 5 is: ");
+        for(Employee emp : employee_in){
+            System.out.println(emp);
+         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
--- criteria/src/main/java/com/example/methodImplementation.java
+++ criteria/src/main/java/com/example/methodImplementation.java
@@ -5,12 +5,14 @@ import java.util.List;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 public class methodImplementation {
 
@@ -86,9 +88,9 @@ public class methodImplementation {
             CriteriaQuery<Employee> criteriaquery = cb.createQuery(Employee.class);
 
             Root<Employee> root = criteriaquery.from(Employee.class);
-            criteriaquery.select(root).where(cb.equal(root.get("employeeId"),18));
+            criteriaquery.select(root).where(cb.equal(root.get("employeeId"), 18));
 
-           employees = session.createQuery(criteriaquery).getResultList();
+            employees = session.createQuery(criteriaquery).getResultList();
 
         } finally {
             session.close();
@@ -98,7 +100,6 @@ public class methodImplementation {
         return employees;
     }
 
-    
     public static List<Employee> employeExperience() {
         Configuration con = new Configuration().configure("hibernate.cfg.xml");
         SessionFactory sf = con.buildSessionFactory();
@@ -107,9 +108,9 @@ public class methodImplementation {
 
         try {
             CriteriaBuilder cb = session.getCriteriaBuilder();
-            CriteriaQuery <Employee> cq = cb.createQuery(Employee.class);
-            Root<Employee> root =  cq.from(Employee.class) ;
-            
+            CriteriaQuery<Employee> cq = cb.createQuery(Employee.class);
+            Root<Employee> root = cq.from(Employee.class);
+
             cq.select(root).where(cb.greaterThan(root.get("experience"), 5));
             employees = session.createQuery(cq).getResultList();
 
@@ -120,4 +121,80 @@ public class methodImplementation {
 
         return employees;
     }
+
+    public static List<Object[]> employeeRange() {
+        Configuration con = new Configuration().configure("hibernate.cfg.xml");
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        List<Object[]> employees = null;
+
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Object[]> cq = cb.createQuery(Object[].class);
+        Root<Employee> root = cq.from(Employee.class);
+
+        cq.multiselect(root.get("name"), root.get("employeeId"), root.get("role"));
+
+        employees = session.createQuery(cq).getResultList();
+        session.close();
+        return employees;
+    }
+
+    public static List<Object[]> groupByExperience() {
+        Configuration con = new Configuration().configure("hibernate.cfg.xml");
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        List<Object[]> employees = null;
+
+        try {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Object[]> cq = cb.createQuery(Object[].class);
+            Root<Employee> root = cq.from(Employee.class);
+
+            cq.multiselect(root.get("experience"), cb.count(root)).groupBy(root.get("experience"));
+            Query<Object[]> query = session.createQuery(cq);
+            employees = query.getResultList();
+        } finally {
+            session.close();
+            sf.close();
+        }
+
+        return employees;
+    }
+
+    public static List<Employee> inOperation(int num1, int num2) {
+        Configuration con = new Configuration().configure("hibernate.cfg.xml");
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        List<Employee> employees = null;
+
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Employee> cq = cb.createQuery(Employee.class);
+
+        Root<Employee> root = cq.from(Employee.class);
+        cq.select(root).where(root.get("salary").in(num1, num2));
+
+        employees = session.createQuery(cq).getResultList();
+
+        return employees;
+    }
+
+    public static List<Employee> betweenOperator() {
+        Configuration con = new Configuration().configure("hibernate.cfg.xml");
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        List<Employee> employees = null;
+
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Employee> cq = cb.createQuery(Employee.class);
+
+        Root<Employee> root = cq.from(Employee.class);
+        Predicate predicate = cb.between(root.get("salary"), 60000, 65000);
+
+        cq.select(root).where(predicate);
+
+        employees = session.createQuery(cq).getResultList();
+
+        return employees;
+    }
+
 }
